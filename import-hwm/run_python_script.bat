cc3333c830f719fa747ae39394f7536e5449e2cf
lydiarosalia <lydia.rosalia@sky.uk>
2020-02-29 22:23:50 +0000

    remove pause

--- import-hwm/run_python_script.bat
+++ import-hwm/run_python_script.bat
@@ -1,2 +1,1 @@
 "C:/ProgramData/Anaconda3/python.exe" "C:/Users/LRS07/OneDrive - Sky/Documents/Others/SME Water Project/code/sme-water-dpm-solution/import-hwm/run_locally.py" --configfile configuration_files.csv --dbname dpm-solution
-pause
